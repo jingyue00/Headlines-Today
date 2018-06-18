ed1cefdae11e1c642cefa8463794312594e20d54
jingyue00 <yuejing3963@gmail.com>
2018-06-18 13:19:57 -0400

    init commit

---
+++ src/main/java/edu/nyu/jingyue00/Headlines/HeadlinesApplication.java
@@ -0,0 +1,12 @@
+package edu.nyu.jingyue00.Headlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlinesApplication.class, args);
+	}
+}
